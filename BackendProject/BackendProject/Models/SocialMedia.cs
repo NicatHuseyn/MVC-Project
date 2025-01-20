a1c9dcbff7dbcbf3e804f0a93f2e0b6a7eee2737
NicatHuseyn <nicat5075@gmail.com>
2025-01-20 22:46:32 +0400

    Create Entity Models and migrate database

---
+++ BackendProject/BackendProject/Models/SocialMedia.cs
@@ -0,0 +1,13 @@
+﻿using BackendProject.Models.Common;
+
+namespace BackendProject.Models
+{
+	public class SocialMedia : BaseEntity
+	{
+		public int Id { get; set; }
+		public string Social { get; set; }
+
+		public int ChefId { get; set; }
+		public Chef Chef { get; set; }
+	}
+}
