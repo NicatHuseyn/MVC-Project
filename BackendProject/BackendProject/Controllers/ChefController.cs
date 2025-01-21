0d2a2f900ff1582a8df9472bb311c084d6ee2cd4
NicatHuseyn <nicat5075@gmail.com>
2025-01-21 05:01:51 +0400

    Account Page, Basket Page,Fag and some pages added

---
+++ BackendProject/BackendProject/Controllers/ChefController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendProject.Controllers
+{
+    public class ChefController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ChefController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Chef> chefs = _context.Chefs
+                .Where(m => !m.IsDeleted)
+                .Include(m => m.SocialMedias)
+                .ToList();
+
+            return View(chefs);
+        }
+    }
+}
